af083e597889e1a6ba07befa25c309d057db2bad
mssjaney <janey.webdev@gmail.com>
2024-01-26 22:52:32 +0100

    generate todo component

---
+++ src/app/todo/todo.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TodoComponent } from './todo.component';
+
+
+
+@NgModule({
+  declarations: [
+    TodoComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TodoModule { }
